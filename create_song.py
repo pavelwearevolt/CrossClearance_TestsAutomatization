3ce09e9fe61245bc67ba8b546101c5f4ab9aad91
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2017-11-11 22:56:38 +0300

    создание теста create_song

---
+++ create_song.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class create_song(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+    
+    def test_create_song(self):
+        success = True
+        wd = self.wd
+        wd.get("https://cross-auth-staging.herokuapp.com/?redirect_uri=https%3A%2F%2Fcross-edit-staging-frontend.herokuapp.com%2F")
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("pavel.kosicin@wearevolt.com")
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys("abcd1234")
+        wd.find_element_by_xpath("//div[@class='sign-in-control-box']//button[.='Log In']").click()
+        wd.find_element_by_css_selector("input.form-control").click()
+        wd.find_element_by_css_selector("input.form-control").clear()
+        wd.find_element_by_css_selector("input.form-control").send_keys("song_A")
+        wd.find_element_by_css_selector("button.btn.btn-green").click()
+        wd.find_element_by_xpath("//div[@class='modal-body']//button[.='song']").click()
+        wd.find_element_by_id("song_iswc_identification_new").click()
+        wd.find_element_by_id("song_iswc_identification_new").clear()
+        wd.find_element_by_id("song_iswc_identification_new").send_keys("Q-548.132.789-0")
+        wd.find_element_by_id("song_iswc_identification_new_add").click()
+        wd.find_element_by_id("song_asap_identification_new").click()
+        wd.find_element_by_id("song_asap_identification_new").clear()
+        wd.find_element_by_id("song_asap_identification_new").send_keys("U70_S4XJQTR.1GW_P")
+        wd.find_element_by_id("song_asap_identification_new_add").click()
+        wd.find_element_by_id("song_ascap_identification_new").click()
+        wd.find_element_by_id("song_ascap_identification_new").clear()
+        wd.find_element_by_id("song_ascap_identification_new").send_keys("GJ_7.031C46TSW58")
+        wd.find_element_by_id("song_ascap_identification_new_add").click()
+        wd.find_element_by_id("song_bmi_identification_new").click()
+        wd.find_element_by_id("song_bmi_identification_new").clear()
+        wd.find_element_by_id("song_bmi_identification_new").send_keys("13_E7C_W68RSI50H_U")
+        wd.find_element_by_id("song_bmi_identification_new_add").click()
+        wd.find_element_by_id("song_sesac_identification_new").click()
+        wd.find_element_by_id("song_sesac_identification_new").clear()
+        wd.find_element_by_id("song_sesac_identification_new").send_keys("EX.2136TH.UIS54C-0")
+        wd.find_element_by_id("song_sesac_identification_new_add").click()
+        wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").click()
+        wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").clear()
+        wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").send_keys("TEST TEXT")
+        wd.find_element_by_xpath("//div[@class='_30d-pYB2dYPjd0XNrEQjVs']//button[.='Send']").click()
+        wd.find_element_by_link_text("Global search").click()
+        wd.find_element_by_css_selector("input.form-control").click()
+        wd.find_element_by_css_selector("input.form-control").clear()
+        wd.find_element_by_css_selector("input.form-control").send_keys("song_A")
+        wd.find_element_by_link_text("Song_A").click()
+        wd.find_element_by_xpath("//nav[@id='rubix-nav-header']/div/div/div[3]/div/ul/li[7]/a/span").click()
+        wd.find_element_by_css_selector("button.btn.btn-success").click()
+        self.assertTrue(success)
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()

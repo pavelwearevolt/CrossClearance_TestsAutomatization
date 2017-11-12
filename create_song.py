452f82aba87704861ae7d9757090ab962337565d
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2017-11-12 18:06:17 +0300

    созданы вспомогательные методы

--- create_song.py
+++ create_song.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -13,65 +12,83 @@ def is_alert_present(wd):
 class create_song(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(30)
     
     def test_create_song(self):
         success = True
         wd = self.wd
-        # open home page
+        self.open_home_page(wd)
+        self.login(wd, username="pavel.kosicin@wearevolt.com", password="abcd1234")
+        self.create(wd, name="song_A", iswc="Q-548.132.789-0", asap="U70_S4XJQTR.1GW_P", ascap="GJ_7.031C46TSW58",
+                    bmi="13_E7C_W68RSI50H_U", sesac="EX.2136TH.UIS54C-0", note="TEST TEXT")
+        self.return_to_global_search(wd)
+        self.find(wd)
+        self.logout(wd)
+        self.assertTrue(success)
+
+
+
+    def open_home_page(self, wd):
         wd.get("https://cross-auth-staging.herokuapp.com/?redirect_uri=https%3A%2F%2Fcross-edit-staging-frontend.herokuapp.com%2F")
-        # login
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("pavel.kosicin@wearevolt.com")
+        wd.find_element_by_name("email").send_keys(username)
         wd.find_element_by_name("password").click()
         wd.find_element_by_name("password").clear()
-        wd.find_element_by_name("password").send_keys("abcd1234")
+        wd.find_element_by_name("password").send_keys(password)
         wd.find_element_by_xpath("//div[@class='sign-in-control-box']//button[.='Log In']").click()
+
+    def create(self, wd, name, iswc, asap, ascap, bmi, sesac, note):
         # create song
         wd.find_element_by_css_selector("input.form-control").click()
         wd.find_element_by_css_selector("input.form-control").clear()
-        wd.find_element_by_css_selector("input.form-control").send_keys("song_A")
+        wd.find_element_by_css_selector("input.form-control").send_keys(name)
         wd.find_element_by_css_selector("button.btn.btn-green").click()
         wd.find_element_by_xpath("//div[@class='modal-body']//button[.='song']").click()
         # song modification
         wd.find_element_by_id("song_iswc_identification_new").click()
         wd.find_element_by_id("song_iswc_identification_new").clear()
-        wd.find_element_by_id("song_iswc_identification_new").send_keys("Q-548.132.789-0")
+        wd.find_element_by_id("song_iswc_identification_new").send_keys(iswc)
         wd.find_element_by_id("song_iswc_identification_new_add").click()
         wd.find_element_by_id("song_asap_identification_new").click()
         wd.find_element_by_id("song_asap_identification_new").clear()
-        wd.find_element_by_id("song_asap_identification_new").send_keys("U70_S4XJQTR.1GW_P")
+        wd.find_element_by_id("song_asap_identification_new").send_keys(asap)
         wd.find_element_by_id("song_asap_identification_new_add").click()
         wd.find_element_by_id("song_ascap_identification_new").click()
         wd.find_element_by_id("song_ascap_identification_new").clear()
-        wd.find_element_by_id("song_ascap_identification_new").send_keys("GJ_7.031C46TSW58")
+        wd.find_element_by_id("song_ascap_identification_new").send_keys(ascap)
         wd.find_element_by_id("song_ascap_identification_new_add").click()
         wd.find_element_by_id("song_bmi_identification_new").click()
         wd.find_element_by_id("song_bmi_identification_new").clear()
-        wd.find_element_by_id("song_bmi_identification_new").send_keys("13_E7C_W68RSI50H_U")
+        wd.find_element_by_id("song_bmi_identification_new").send_keys(bmi)
         wd.find_element_by_id("song_bmi_identification_new_add").click()
         wd.find_element_by_id("song_sesac_identification_new").click()
         wd.find_element_by_id("song_sesac_identification_new").clear()
-        wd.find_element_by_id("song_sesac_identification_new").send_keys("EX.2136TH.UIS54C-0")
+        wd.find_element_by_id("song_sesac_identification_new").send_keys(sesac)
         wd.find_element_by_id("song_sesac_identification_new_add").click()
         wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").click()
         wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").clear()
-        wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").send_keys("TEST TEXT")
+        wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").send_keys(note)
         wd.find_element_by_xpath("//div[@class='_30d-pYB2dYPjd0XNrEQjVs']//button[.='Send']").click()
-        # return to the global search page
+
+    def return_to_global_search(self, wd):
         wd.find_element_by_link_text("Global search").click()
+
+    def find(self, wd):
         # find created song
         wd.find_element_by_css_selector("input.form-control").click()
         wd.find_element_by_css_selector("input.form-control").clear()
         wd.find_element_by_css_selector("input.form-control").send_keys("song_A")
         # choose found song
         wd.find_element_by_link_text("Song_A").click()
+
+    def logout(self, wd):
         # logout
         wd.find_element_by_xpath("//nav[@id='rubix-nav-header']/div/div/div[3]/div/ul/li[7]/a/span").click()
         wd.find_element_by_css_selector("button.btn.btn-success").click()
-        self.assertTrue(success)
-    
+
     def tearDown(self):
         self.wd.quit()
 

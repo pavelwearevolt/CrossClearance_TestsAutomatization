dbafa243f658e2c89ddd43f5b58c6fdbe9428f7a
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2017-11-13 15:05:05 +0300

    создан файл conftest для создание fixture для всех тестовых методов

---
+++ test/conftest.py
@@ -0,0 +1,10 @@
+__author__ = 'Pavel Kosicin'
+import pytest
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture

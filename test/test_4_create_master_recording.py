ebcbdd73a687b53e7f7f87b07375294e966e61a5
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2017-11-20 06:21:20 +0300

    добавлены вспомогательные классы Recording, Modify

--- test/test_4_create_master_recording.py
+++ test/test_4_create_master_recording.py
@@ -1,10 +1,12 @@
 __author__ = 'Pavel Kosicin'
+from model.recording import Recording
+from model.recording import Modify
 
 
 def test_create_master_recording(app):
     app.session.login(username="pavel.kosicin@wearevolt.com", password="abcd1234")
     app.song.find(name="song_A")
-    app.recording.add_artist()
+    app.recording.add_artist(Recording(artist_name="ra_#1", recording_name="mr_#1"))
     app.recording.find(name="mr_#1")
-    app.recording.modify()
+    app.recording.modify(Modify(version="test_version", isrc="QW-RE1-42-05634", asap="D7J_W9MZ38.5BA_L", note="Test note for master recording"))
     app.session.logout()

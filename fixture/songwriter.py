062da303b2955322bad28cc4be173e9b2e99aadf
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2017-11-20 17:14:50 +0300

    добавлены вспомогательные методы для создания songwriter

--- fixture/songwriter.py
+++ fixture/songwriter.py
@@ -1,4 +1,5 @@
 __author__ = 'Pavel Kosicin'
+import time
 
 
 class SongwriterHelper:
@@ -6,7 +7,7 @@ class SongwriterHelper:
     def __init__(self, app):
         self.app = app
 
-    def create(self, songwriter):
+    def create_from_song(self, songwriter):
         wd = self.app.wd
         # open tab songwriter
         wd.find_element_by_link_text("Songwriters").click()
@@ -17,14 +18,60 @@ class SongwriterHelper:
         wd.find_element_by_id("writing_new_create").click()
         wd.find_element_by_link_text(songwriter.name).click()
         # fill identifications
-        self.fill_identificators(songwriter)
+        self.fill_fields(songwriter)
+        wd.find_element_by_css_selector("button.btn.btn-success").click()
         # find created songwriter, open songwriter edit page
         self.app.navigate.menu_people()
         self.find(name="sw_#1")
         # add notes
         self.add_notes(songwriter)
 
-    def fill_identificators(self, songwriter):
+    def create_menu_global_search(self, songwriter):
+        wd = self.app.wd
+        # create songwriter
+        wd.find_element_by_css_selector("input.form-control").click()
+        wd.find_element_by_css_selector("input.form-control").clear()
+        wd.find_element_by_css_selector("input.form-control").send_keys(songwriter.name)
+        wd.find_element_by_css_selector("button.btn.btn-green").click()
+        wd.find_element_by_xpath("//div[@class='NTialWg8eHGF9sNIWyhYH']//button[.='person']").click()
+        # select person type
+        self.type()
+        # find created songwriter
+        self.app.navigate.menu_people()
+        self.find(name="sw_#2")
+        # fill fields on the songwriter edit page
+        self.fill_fields(songwriter)
+        # add notes
+        self.add_notes(songwriter)
+
+    def create_menu_people(self, songwriter):
+        wd = self.app.wd
+        # navigate menu people
+        self.app.navigate.menu_people()
+        # create songwriter
+        wd.find_element_by_css_selector("input.form-control").click()
+        wd.find_element_by_css_selector("input.form-control").clear()
+        wd.find_element_by_css_selector("input.form-control").send_keys(songwriter.name)
+        wd.find_element_by_css_selector("button.btn.btn-green").click()
+        wd.find_element_by_css_selector("button.btn.btn-success").click()
+        # select person type
+        self.type()
+        # find created songwriter
+        self.app.navigate.menu_people()
+        self.find(name="sw_#3")
+        # fill fields on the songwriter edit page
+        self.fill_fields(songwriter)
+        # add notes
+        self.add_notes(songwriter)
+
+    def type(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//form[@class='form-horizontal']/div[3]/div/div[1]/div/button").click()
+        wd.find_element_by_xpath("//form[@class='form-horizontal']/div[3]/div/div[1]/div/ul/li[1]").click()
+        wd.find_element_by_css_selector("button.btn.btn-success").click()
+        time.sleep(3)
+
+    def fill_fields(self, songwriter):
         wd = self.app.wd
         wd.find_element_by_id("songwriter_ipicae_identification_new").click()
         wd.find_element_by_id("songwriter_ipicae_identification_new").clear()
@@ -34,7 +81,6 @@ class SongwriterHelper:
         wd.find_element_by_id("songwriter_asap_identification_new").clear()
         wd.find_element_by_id("songwriter_asap_identification_new").send_keys(songwriter.asap)
         wd.find_element_by_id("songwriter_asap_identification_new_add").click()
-        wd.find_element_by_css_selector("button.btn.btn-success").click()
 
     def find(self, name):
         wd = self.app.wd
@@ -45,7 +91,8 @@ class SongwriterHelper:
 
     def add_notes(self, songwriter):
         wd = self.app.wd
-        wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").click()
-        wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").clear()
-        wd.find_element_by_css_selector("textarea.form-control._3N_4MUdz6Is-muISLxDGRP").send_keys(songwriter.note)
-        wd.find_element_by_xpath("//div[@class='_30d-pYB2dYPjd0XNrEQjVs']//button[.='Send']").click()
+        wd.find_element_by_xpath("//form[@class='form-horizontal']/div[6]/div/div/div[1]/textarea").click()
+        wd.find_element_by_xpath("//form[@class='form-horizontal']/div[6]/div/div/div[1]/textarea").clear()
+        wd.find_element_by_xpath("//form[@class='form-horizontal']/div[6]/div/div/div[1]/textarea").send_keys(songwriter.note)
+        wd.find_element_by_xpath("//form[@class='form-horizontal']/div[6]/div/div/div[1]/button").click()
+        time.sleep(3)

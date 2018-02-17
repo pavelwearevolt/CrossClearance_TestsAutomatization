071e33925fc89b29d162fd4622f5267472429130
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2018-02-17 18:22:53 +0300

    добавлены методы навигации на странице редактирования песни

--- fixture/navigate.py
+++ fixture/navigate.py
@@ -14,6 +14,26 @@ class NavigateHelper:
         wd = self.app.wd
         wd.find_element_by_name("Global search").click()
 
+    def tab_general_info(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("General Info").click()
+
     def tab_songwriters(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Songwriters").click()
+
+    def tab_publishers(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Publishers").click()
+
+    def tab_directives(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Directives").click()
+
+    def tab_copyrigt_collective_info(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Copyright Collective Info").click()
+
+    def tab_master_recordings(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Master Recordings").click()

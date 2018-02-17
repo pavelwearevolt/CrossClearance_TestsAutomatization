dce3e91065e61cbaae23407bc4858cbd1cd86e4e
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2018-02-17 18:23:20 +0300

    отредактирован вспомогательный класс modify

--- model/modify.py
+++ model/modify.py
@@ -3,11 +3,10 @@ __author__ = 'pavelkosicin'
 
 class Modify:
 
-    def __init__(self, name=None, iswc=None, asap=None, ascap=None, bmi=None, sesac=None, note=None):
+    def __init__(self, name=None, iswc=None, asap=None, ascap=None, bmi=None, sesac=None):
         self.name = name
         self.iswc = iswc
         self.asap = asap
         self.ascap = ascap
         self.bmi = bmi
         self.sesac = sesac
-        self.note = note

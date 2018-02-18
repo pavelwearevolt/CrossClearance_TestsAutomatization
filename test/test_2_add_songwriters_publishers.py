8ef5b005043dff18e93a6f2adc58a4b50da765ee
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2018-02-18 18:22:30 +0300

    добавлен тест редактирования publisher в модальном окне на странице редактирования песни

--- test/test_2_add_songwriters_publishers.py
+++ test/test_2_add_songwriters_publishers.py
@@ -1,9 +1,6 @@
 __author__ = 'pavelkosicin'
 from model.songwriter import Songwriter
-
-
-def test_search_song(app):
-    app.song.search_song(query="song_A")
+from model.publisher import Publisher
 
 
 def test_create_songwriter_in_song(app):
@@ -19,17 +16,34 @@ def test_edit_songwriter_in_song(app):
     app.song.check_cross_id(locator_path="div._15tqGvIentb7leltuBfTAA>div>div.col-xs-9>div", prefix='CCSW')
     app.song.add_secondary_name(new_name="new_name_person_songwriter_#1", text="writing")
     app.song.edit_songwriter_roles()
-    app.song.fill_identifiers_fields_in_edit_songwriter_modal_window(Songwriter(
+    app.song.fill_identifiers_fields_in_edit_entity_modal_window(Songwriter(
         ipicae="G-332947321-0",
         asap="4443588649923"
-        ))
-    app.song.edit_sonwriter_info_in_modal_window(Songwriter(
+        ), entity="songwriter")
+    app.song.edit_entity_info_in_modal_window(Songwriter(
         name="edited_name_person_songwriter_#1",
         ipicae="T-332947321-2",
         asap="4443588649922"
-        ))
+        ), entity_name="writing", entity_identifier="songwriter")
 
 
 def test_create_publisher_in_song(app):
     app.navigate.tab_publishers()
     app.song.add_entity_link(name="company_publisher_#1", text="publishing")
+
+
+def test_edit_publisher_in_song(app):
+    app.song.open_edit_entity_modal_window()
+    app.song.close_edit_entity_modal_window()
+    app.song.open_edit_entity_modal_window()
+    app.song.check_cross_id(locator_path="div._15tqGvIentb7leltuBfTAA>div>div.col-xs-9>div", prefix='CCPB')
+    app.song.add_secondary_name(new_name="new_name_company_publisher_#1", text="publishing")
+    app.song.fill_identifiers_fields_in_edit_entity_modal_window(Publisher(
+        ipicae="Q-562975093-1",
+        asap="566487389203"
+        ), entity="publisher")
+    app.song.edit_entity_info_in_modal_window(Publisher(
+        name="edited_name_company_publisher_#1",
+        ipicae="H-562975093-2",
+        asap="566487389202"
+        ), entity_name="publishing", entity_identifier="publisher")

540fa29db7d49435b09d21701f9e35f990ec8726
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2018-02-18 10:19:46 +0300

    отредактированы классы помощники songwriter, publisher

--- model/publisher.py
+++ model/publisher.py
@@ -3,7 +3,7 @@ __author__ = 'pavelkosicin'
 
 class Publisher:
 
-    def __init__(self, name, ipicae, asap, note):
+    def __init__(self, name=None, ipicae=None, asap=None, note=None):
         self.name = name
         self.ipicae = ipicae
         self.asap = asap
--- model/songwriter.py
+++ model/songwriter.py
@@ -4,7 +4,7 @@ __author__ = 'pavelkosicin'
 class Songwriter:
 
     def __init__(self, name=None, ipicae=None, asap=None, note=None):
-        self. name = name
+        self.name = name
         self.ipicae = ipicae
         self.asap = asap
         self.note = note
